4fd44e05d77830aa73db951ff3d475abceca060e
diegoDev <diegopd98@hotmail.com>
2022-06-25 16:49:30 -0300

    Created alumnos interface and showing only students age over 18

---
+++ src/app/components/main/main.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListaDeAlumnosComponent } from '../lista-de-alumnos/lista-de-alumnos.component';
+
+
+@NgModule({
+  declarations: [ListaDeAlumnosComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainModule { }
