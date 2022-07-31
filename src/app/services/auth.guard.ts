bff80c2d4970b8751332b3ef993e8759c7dffb51
Diego-Perez <diegopd98@hotmail.com>
2022-07-31 20:11:25 -0300

    Added auth guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (true) {
+      return this.router.navigate(['/login']).then(() => false);
+    }
+    return true;
+  }
+}
