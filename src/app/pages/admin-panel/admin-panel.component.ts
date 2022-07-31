f4a2d94aa4002ce393d4afbb1931aa88e094838e
Diego-Perez <diegopd98@hotmail.com>
2022-07-31 19:16:50 -0300

    modularized code

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,67 +1,73 @@
-import { Component, ElementRef, OnInit ,ViewChild} from '@angular/core';
-import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { BehaviorSubject, Observable, of, switchMap } from 'rxjs';
-import { EditarAlumnoFormComponent } from 'src/app/components/editar-alumno-form/editar-alumno-form.component';
-import { EditarAlumnoModalComponent } from 'src/app/components/editar-alumno-modal/editar-alumno-modal.component';
+
 import { Alumno } from 'src/app/interfaces/alumnos';
 import { RequestsService } from 'src/app/services/requests.service';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.scss']
+  styleUrls: ['./admin-panel.component.scss'],
 })
-export class AdminPanelComponent implements OnInit{
-  @ViewChild('content') content:any;
-  @ViewChild('editarAlumnoModal') editarAlumnoModal:any;
+export class AdminPanelComponent implements OnInit {
+  @ViewChild('content') content: any;
+  @ViewChild('editarAlumnoModal') editarAlumnoModal: any;
   closeResult = '';
-  showModal:boolean = false;
-  infoAlumno:Alumno = {nombre:'',apellido:'',edad:0,id:0}
-  idCount:number = 0;
-  
-  alumnos$:Observable<Alumno[]>=of([]);
-  refreshStudents$ =new BehaviorSubject<boolean>(true)
+  showModal: boolean = false;
+  infoAlumno: Alumno = { nombre: '', apellido: '', edad: 0, id: 0 };
+  idCount: number = 0;
 
-  constructor(private requests:RequestsService,private modalService: NgbModal) { 
-  }
-  
+  alumnos$: Observable<Alumno[]> = of([]);
+  refreshStudents$ = new BehaviorSubject<boolean>(true);
+
+  constructor(
+    private requests: RequestsService,
+    private modalService: NgbModal
+  ) {}
+
   ngOnInit(): void {
-     this.alumnos$ = this.refreshStudents$.pipe(switchMap(_=>this.requests.getStudents()))
-   }
-   
-  newAlumno(alumno:Alumno){
-    this.requests.newStudent(alumno).subscribe(()=>{
-      console.log('New Student created')
-      this.refreshStudents$.next(!this.refreshStudents$)
-    })
+    this.alumnos$ = this.refreshStudents$.pipe(
+      switchMap((_) => this.requests.getStudents())
+    );
+  }
 
+  newAlumno(alumno: Alumno) {
+    this.requests.newStudent(alumno).subscribe(() => {
+      console.log('New Student created');
+      this.refreshStudents$.next(!this.refreshStudents$);
+    });
   }
- 
-  modificarAlumno(alumno:Alumno){
-    this.requests.updateStudent(alumno).subscribe((res)=>{
-      this.refreshStudents$.next(!this.refreshStudents$)
-      this.modalService.dismissAll()
-    })
+
+  modificarAlumno(alumno: Alumno) {
+    this.requests.updateStudent(alumno).subscribe((res) => {
+      this.refreshStudents$.next(!this.refreshStudents$);
+      this.modalService.dismissAll();
+    });
   }
-  
-  deleteAlumno(id:number){
-    this.requests.deleteStudent(id).subscribe((res)=>{
-      this.refreshStudents$.next(!this.refreshStudents$)
-      this.modalService.dismissAll()
-    })
+
+  deleteAlumno(id: number) {
+    this.requests.deleteStudent(id).subscribe((res) => {
+      this.refreshStudents$.next(!this.refreshStudents$);
+      this.modalService.dismissAll();
+    });
   }
-  setearAlumnoAModificar(alumno:Alumno){
-    console.log(alumno)
-    this.infoAlumno = alumno
-    this.open()
-   
+  setearAlumnoAModificar(alumno: Alumno) {
+    console.log(alumno);
+    this.infoAlumno = alumno;
+    this.open();
   }
   open() {
-    this.modalService.open(this.content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService
+      .open(this.content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
