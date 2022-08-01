7047ef063ede667ab295d2da0fa313dfa6c760d0
Diego-Perez <diegopd98@hotmail.com>
2022-07-31 22:02:59 -0300

    Implemented login system with Guard

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -7,12 +7,17 @@ import {
   Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../modules/auth/services/authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  loggedIn: boolean = false;
+  constructor(private router: Router, private auth: AuthenticationService) {}
+  setLoggedIn(e: boolean) {
+    this.loggedIn = e;
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -21,7 +26,7 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (true) {
+    if (!this.loggedIn) {
       return this.router.navigate(['/login']).then(() => false);
     }
     return true;
